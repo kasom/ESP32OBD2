ed73e4bf5f0e9cbe7bf31ce33dfd61e2cb324efb
Kasom Koht-arsa <kasom.k@gmail.com>
2023-09-17 10:10:05 +0700

    Like previous update :P

--- data/firmware/display.js
+++ data/firmware/display.js
@@ -474,7 +474,7 @@ function doNextRequest() {
 
 		drawGridValue1(d.disp.c, d.disp.r, d.format(d.valueMin), getValueTextColor(name,1,d.valueMin), getValueTextBgColor(name,1,d.valueMin));
 		drawGridValue2(d.disp.c, d.disp.r, d.format(d.value), getValueTextColor(name,2,d.value), getValueTextBgColor(name,2,d.value));
-		drawGridValue3(d.disp.c, d.disp.r, d.format(d.valueMax), getValueTextColor(name,3,d.valueMin), getValueTextBgColor(name,3,d.valueMax));
+		drawGridValue3(d.disp.c, d.disp.r, d.format(d.valueMax), getValueTextColor(name,3,d.valueMax), getValueTextBgColor(name,3,d.valueMax));
 
 		toDraw.shift();
 	}
